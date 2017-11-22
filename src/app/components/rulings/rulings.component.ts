6dbdff808286f2cea39d5260e893dda735645b29
sebaspa <sebaspa0107@gmail.com>
2017-11-22 03:34:43 -0500

    Crear el componente rulings

---
+++ src/app/components/rulings/rulings.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-rulings',
+	templateUrl: './rulings.component.html'
+})
+export class RulingsComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+}
