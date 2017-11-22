2dd24d6578f84c8b07ff58ce5e16246965bbe7c5
sebaspa <sebaspa0107@gmail.com>
2017-11-21 23:38:09 -0500

    Agrega el componente home y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { app_routing } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		app_routing
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
